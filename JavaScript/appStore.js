999a5fa6e8b5942b5c66664ce8bf847a57821b5a
Ashish Gundecha <ashishgundecha92@gmail.com>
2023-07-26 10:33:26 +0530

    Update appStore.js

--- JavaScript/appStore.js
+++ JavaScript/appStore.js
@@ -1,31 +1,43 @@
 (function () {
    
-    var appStore = {
+    
+    var appCookieStore = {
         get: function (key) {
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${key}=`);
-            if (parts.length === 2) return parts.pop().split(';').shift();
+            return Cookies.get(key);
         },
         save: function (key, value) {
-            var expiry = value.includes("Bearer") ? application.JWTToken.getTokenExpiryTime(value) : application.JWTToken.getTokenExpiryTime(application.appStore.get("api-access-token"));
-            document.cookie = key + "=" + value + ";expires=" + expiry.toUTCString() + ";path = /";
+            var expiry = value.includes("Bearer") ? Application.JWTToken.getTokenExpiryTime(value) : Application.JWTToken.getTokenExpiryTime(Application.appCookieStore.get("api-access-token"));
+            Cookies.set(key, value, { expires: expiry, path: '/' });
         },
         delete: function (key) {
-            document.cookie = key + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            Cookies.remove(key);
         },
         clear: function () {
-            var cookies = document.cookie.split(";");
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = cookies[i];
-                var eqPos = cookie.indexOf("=");
-                var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            var cookies = Cookies.get()
+            for (var cookie in cookies) {
+                Application.appCookieStore.delete(cookie);
             }
         },
     };
 
-    window.application = $.extend(true, window.application, {
-        "appStore": appStore
+    var appSessionStore = {
+        get: function (key) {
+            return sessionStorage.getItem(key);
+        },
+        save: function (key, value) {
+            return sessionStorage.setItem(key, value);
+        },
+        delete: function (key) {
+            sessionStorage.removeItem(key);
+        },
+        clear: function () {
+            sessionStorage.clear();
+        },
+    };
+
+    window.Application = $.extend(true, window.Application, {
+        "appSessionStore": appSessionStore,
+        "appCookieStore": appCookieStore
     });
 
 })();
